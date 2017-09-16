35b6cca6cd41ac55f5cc09b6620f2f22a1e6cc2a
Tóth András <tandris@Bandis-iMac-PC.local>
2017-09-16 22:11:22 +0200

    add resolve method

--- Service.js
+++ Service.js
@@ -1,5 +1,6 @@
 const nsqjs = require('nsqjs');
 const ServiceManager = require('./index');
+const conc = require('concordant')();
 
 class Service {
   configure(config, cb) {
@@ -16,6 +17,29 @@ class Service {
 
   sendMessage(topic, message) {
     return ServiceManager.sendNsqMessage(topic, message);
+  }
+
+  resolve(service, host) {
+    if (process.env[service + '_SERVICE_HOST'] && process.env[service + '_SERVICE_PORT']) {
+      let host = process.env[service + '_SERVICE_HOST'];
+      let port = process.env[service + '_SERVICE_PORT'];
+      console.log('Resolve service from environment variable. { service = ' + service + '; host = ' + host + '; port = ' + port + ' }');
+      return Promise.resolve({
+        host: host,
+        port: port,
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        conc.dns.resolve(host, function (err, result) {
+          if (err) {
+            winston.error('Failed to resolve host. { host = ' + host + ' }', err);
+            reject(err);
+          } else {
+            resolve(result[0]);
+          }
+        });
+      });
+    }
   }
 }
 
--- db/MongoDbService.js
+++ db/MongoDbService.js
@@ -17,7 +17,7 @@ class MongoDbService extends Service {
 
     this._config = config;
 
-    this._resolve(config.host)
+    this.resolve('MONGO', config.host)
       .then(({host, port}) => {
         winston.info('MongoDB connection. { url = ' + 'mongodb://' + host + ':' + port + '/' + config.dbName + ' }');
         self._connection = mongoose.createConnection('mongodb://' + host + ':' + port + '/' + config.dbName, {
@@ -32,29 +32,6 @@ class MongoDbService extends Service {
 
   init(cb) {
     cb();
-  }
-
-  _resolve(host) {
-    if (process.env['MONGO_SERVICE_HOST'] && process.env['MONGO_SERVICE_PORT']) {
-      let host = process.env['MONGO_SERVICE_HOST'];
-      let port = process.env['MONGO_SERVICE_PORT'];
-      console.log('Resolve service from environment variable. { host = ' + host + '; port = ' + port + ' }');
-      return Promise.resolve({
-        host: host,
-        port: port,
-      });
-    } else {
-      return new Promise((resolve, reject) => {
-        conc.dns.resolve(host, function (err, result) {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            resolve(result[0]);
-          }
-        });
-      });
-    }
   }
 
   get connection() {
